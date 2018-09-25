e48cbb1344ccd1a6de108df2367c06dda22e9d28
Vagner Oliveira <vagner.olliver@gmail.com>
2018-09-25 03:22:10 -0300

    limar template.html

--- src/app/workflowgroup/components/card-metadata/card-metadata.component.ts
+++ src/app/workflowgroup/components/card-metadata/card-metadata.component.ts
@@ -3,19 +3,20 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
     selector: 'app-card-metadata',
     template: `
-    <div class="card">
-        <header class="card-header">
-            <p class="card-header-title">
-                {{ title }}
-            </p>
-            <a class="card-header-icon">
-                <span class="icon">
-                    <i class="fa fa-angle-down"></i>
-                </span>
-            </a>
-        </header>
-        <ng-content></ng-content>
-    </div>`,
+        <div class="card">
+            <header class="card-header">
+                <p class="card-header-title">
+                    {{ title }}
+                </p>
+                <a class="card-header-icon">
+                    <span class="icon">
+                        <i class="fa fa-angle-down"></i>
+                    </span>
+                </a>
+            </header>
+            <ng-content></ng-content>
+        </div>
+    `,
     styleUrls: ['./card-metadata.component.sass']
 })
 export class CardMetadataComponent implements OnInit {
--- src/app/workflowgroup/components/fields-input-text/fields-input-text.component.ts
+++ src/app/workflowgroup/components/fields-input-text/fields-input-text.component.ts
@@ -3,7 +3,19 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'app-fields-input-text',
-    templateUrl: './fields-input-text.component.html',
+    template: `
+        <label class="label">{{ label }}</label>
+        <div class="control is-expanded has-icons-left">
+            <input class="input"
+                [value]="value"
+                (keyup)="onKeyUp($event.target.value)"
+                type="text"
+                [placeholder]="placeholder">
+            <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+            </span>
+        </div>
+    `,
     styleUrls: ['./fields-input-text.component.sass'],
     providers: [
         {
